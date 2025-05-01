ce9f93010cf2df709fbd2ad6ee8b441bdf0f7c0f
ahlleah <inthewindm@gmail.com>
2025-05-01 20:55:25 +0800

    Updates

--- assets/ios.transition-legacy-BIoDun_r.js
+++ assets/ios.transition-legacy-BIoDun_r.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ClPXIUQH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-NvNhoPQl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-B0JA0sLm.js
+++ assets/swipe-back-legacy-B0JA0sLm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ClPXIUQH.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-NvNhoPQl.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
