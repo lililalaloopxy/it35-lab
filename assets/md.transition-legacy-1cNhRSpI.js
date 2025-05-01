2fa6f026a651da094a18e673f7919503334890d9
ahlleah <inthewindm@gmail.com>
2025-05-01 20:43:18 +0800

    Updates

--- assets/md.transition-legacy-1cNhRSpI.js
+++ assets/md.transition-legacy-1cNhRSpI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ZDhwjXYx.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-ClPXIUQH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
