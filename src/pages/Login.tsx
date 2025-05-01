4111be8227eb290a9e29b1655e5a46d5f938c808
ahlleah <inthewindm@gmail.com>
2025-05-01 20:26:15 +0800

    updated pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -71,7 +71,7 @@ const Login: React.FC = () => {
             }}
           >
             <img 
-             src="https://scontent.fmnl9-4.fna.fbcdn.net/v/t1.15752-9/494818758_1345494313395862_122445321094775502_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeHnYO_tvJliIjP2FqfQPfYfmCsESM1VLTGYKwRIzVUtMeSuhgLaaE0PzH0E4mdomglCPUk97UoUKonhJUDBLqhr&_nc_ohc=DW9duO0IylgQ7kNvwHYyayb&_nc_oc=AdnW49F14sAZ3DR3YGzwXqQumGTqttGpA98M7ywifMB3rSC3o3JCEhq_vrecwtziyWI&_nc_ad=z-m&_nc_cid=1066&_nc_zt=23&_nc_ht=scontent.fmnl9-4.fna&oh=03_Q7cD2AHYjdeQGpA7EOWaAbSiVgM5K2N81p9Jujn788nOvcS53Q&oe=683ACDEF" 
+             src="https://scontent.fmnl9-6.fna.fbcdn.net/v/t1.15752-9/494576848_23899999479597700_8614270150901344207_n.jpg?stp=dst-jpg_p480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeEFSaEQ5HE73HcpWGrUw1mchpsED1k7fceGmwQPWTt9x95Xg99A_Bl3lcHqn9U_TMXz8mcyfRRnhfD1bkF-Zqsw&_nc_ohc=nYNhmGs6CXUQ7kNvwHuXD7M&_nc_oc=AdnRJ7eIrF0uw_YDrZl8mX48Ey2tvVIMjmqe-HE4LaVVqWanM92K9dZfDBYhPDPCnJg&_nc_ad=z-m&_nc_cid=1066&_nc_zt=23&_nc_ht=scontent.fmnl9-6.fna&oh=03_Q7cD2AESXMDAKgZPNT0h4VNhgvrmn8dLjFiaXh5gPeYM_oWtYw&oe=683AC4AC" 
              alt="User Avatar" 
              style={{ width: '100%', height: '100%' }} 
             />
