23a3c2e916a3b3d47780b4d635d65dd3fb8ca6fd
jonard <jonardpasilan106@gmail.com>
2025-05-23 17:22:47 +0800

    added feed & fixed editprofile

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Home from './pages/Home';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
 import Register from './pages/Register';
@@ -42,10 +41,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/it35-lab" component={Login}/>
+        <Route exact path="/it35-lab" component={Login} />
         <Route exact path="/it35-lab/register" component={Register} />
-        <Route exact path="/it35-lab/app" component={Menu}/>
-
+        <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
