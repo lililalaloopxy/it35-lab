ff63a104d341937a58c516199807870b28c7d42c
jonard <jonardpasilan106@gmail.com>
2025-05-23 17:32:04 +0800

    Updates

--- assets/ios.transition-legacy-ktxu_Dt7.js
+++ assets/ios.transition-legacy-ktxu_Dt7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-NvNhoPQl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B3KSpRWo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
